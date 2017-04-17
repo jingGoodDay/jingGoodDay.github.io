a35707d2a0b6db6564ce0dd815b010c5e2ff8e5f
jingGoodDay <878385644@qq.com>
2017-04-17 17:23:25 +0800

    Site updated: 2017-04-17 17:23:24

--- sitesource/intelligentVisualization/js/index.js
+++ sitesource/intelligentVisualization/js/index.js
@@ -1,8 +1,8 @@
 var photos= new Array();
-photos[0]="../../../../img/visual1.png";
-photos[1]="../../../../img/visual2.png";
-photos[2]="../../../../img/visual3.png";
-photos[3]="../../../../img/visual4.png";
+photos[0]="../../../../img/introduction/visual1.jpg";
+photos[1]="../../../../img/introduction/visual2.jpg";
+photos[2]="../../../../img/introduction/visual3.jpg";
+photos[3]="../../../../img/introduction/visual4.jpg";
 
 $(function(){
     pictures.init();
